8e48838dcbcd07087d50d2352624619900f74ae6
akli0027 <73159374+akli0027@users.noreply.github.com>
2021-06-30 13:20:25 +0700

    Update config.php

--- include/config.php
+++ include/config.php
@@ -1,8 +1,5 @@
 <?php
-define('DB_HOST', 'localhost');
-define('DB_USER','root');
-define('DB_PASS' ,'');
-define('DB_NAME', 'ipuser');
+$con = new mysqli ('db4free.net','laptop_bekas','b5a7c9f2','laptop_bekas');
 $con = mysqli_connect(DB_HOST,DB_USER,DB_PASS,DB_NAME);
 // Check connection
 if (mysqli_connect_errno())
